e2c6bb9829556069bd9f201dc42edb679f93d55b
LukaGrdinic <1lukagrdinic@gmail.com>
2018-07-17 01:34:32 +0200

    Add simonArray and userArray functionality

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,19 @@
 
 var powerButton = document.getElementById('powerButton');
 var startButton = document.getElementById('startButton');
+
+/* Color Shapes Buttons */
+
+var colorShapes = document.querySelectorAll('.colorShape');
+
 var counter = document.getElementById('counter');
 
 var game = {
+    'powerOn': false,
     'level': 0,
-    'simonArray': []
+    'userReady': false,
+    'simonArray': [],
+    'userArray': []
 }
 
 
@@ -16,15 +24,44 @@ var game = {
 
 powerButton.addEventListener('input', function () {
     if (powerButton.checked) {
+        game.powerOn = true;
         counter.textContent = '-';
     } else {
+        game.powerOn = false;
         counter.textContent = '';
     }
 });
 
 startButton.addEventListener('click', function () {
-    if (powerButton.checked) {
+    if (game.powerOn) {
         console.log('Start or Reset the Game');
+        pickRandomColor();
+        game.userReady = true;
         counter.textContent = '0';
     }
 });
+
+colorShapes.forEach(function(colorShape) {
+    colorShape.addEventListener('click', function() {
+        if (game.userReady) {
+            game.userArray.push(this.id);
+        }
+    });
+});
+
+/* GAME FUNCTIONS */
+
+function pickRandomColor() {
+    var randomNumber = Math.random();
+
+    if (randomNumber <= 0.25) {
+        game.simonArray.push('greenButton');
+    } else if (0.25 < randomNumber && randomNumber <= 0.5) {
+        game.simonArray.push('redButton');
+    } else if (0.5 < randomNumber && randomNumber <= 0.75) {
+        game.simonArray.push('blueButton');
+    } else if (0.75 < randomNumber && randomNumber <= 1) {
+        game.simonArray.push('yellowButton');
+    }
+
+}
