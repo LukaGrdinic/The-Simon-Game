c4f99b0a6921b1b5bca86c1365596ef646a5c111
LukaGrdinic <1lukagrdinic@gmail.com>
2018-07-16 21:27:00 +0200

    Add startButton functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,30 @@
+/* VARIABLES */
+
 var powerButton = document.getElementById('powerButton');
+var startButton = document.getElementById('startButton');
 var counter = document.getElementById('counter');
+
+var game = {
+    'level': 0,
+    'simonArray': []
+}
+
+
+/* ADD EVENT HANDLERS */
 
 /* If powerButton.checked , then the game is on */
 
-powerButton.addEventListener('input', function() {
+powerButton.addEventListener('input', function () {
     if (powerButton.checked) {
         counter.textContent = '-';
     } else {
-        counter.textContent= '';
+        counter.textContent = '';
+    }
+});
+
+startButton.addEventListener('click', function () {
+    if (powerButton.checked) {
+        console.log('Start or Reset the Game');
+        counter.textContent = '0';
     }
 });
